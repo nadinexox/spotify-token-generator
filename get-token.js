ec2144a2ba8d7824233089cdb1b66f3a2a283c6e
luigiajah <luigiajah@gmail.com>
2025-01-13 23:22:55 +0100

    Updated Readme

--- get-token.js
+++ get-token.js
@@ -2,9 +2,9 @@
 import fetch from 'node-fetch';
 
 async function getRefreshToken() {
-    const clientId = '3764500d720b4a1ea9628f19ebb2f540';
-    const clientSecret = '620dd1be5df847569825f660b6f69f23';
-    const code = 'AQAhjDwZLGsfY0e7VDdU1iYenApaLvjInzFynr-449Gzg92IJsOcAVaMsJ92vIXDVbdg-YhH-OVDhk9S7QY_AGmRykNoQN9qhYiS9LGlWBXrh0G4KEBZGLyRlNgXPvVSm9x3NDTw8G4EI3CNbOigFx0NJVyIoSY_tAh3PyZlyWBRL3GrZxvQhSNCl2NTvb6ypdZqO1ulKky-XfdzuJ2fNg-xOZWLo_WSJ-mckbuoII8HPKwUa4Zc8BRU';
+    const clientId = 'CLIENT_ID';
+    const clientSecret = 'CLIENT_SECRET';
+    const code = 'CODE';
 
     try {
         const response = await fetch('https://accounts.spotify.com/api/token', {
