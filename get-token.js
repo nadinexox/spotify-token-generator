7340444deda4764c0393a9153e79e343f58b3a6e
Nadine Nguyen <97496089+nadinexox@users.noreply.github.com>
2026-03-15 22:58:59 -0700

    Update get-token.js

--- get-token.js
+++ get-token.js
@@ -2,9 +2,9 @@
 import fetch from 'node-fetch';
 
 async function getRefreshToken() {
-    const clientId = 'CLIENT_ID';
-    const clientSecret = 'CLIENT_SECRET';
-    const code = 'CODE';
+    const clientId = 'b5aa5def8d74432892195089fb9a025b';
+    const clientSecret = '0f81f7544d644741940577d405d4aa2a';
+    const code = 'AQCeeFU4P1t9KnwtI7ZnUqIDQ9_pqyEG9SvPV9pvqFGGfsHeu2JNuiNnaH0XToW8Tt5K8ZjKmhr-QAvz4osRLs36I2SxOS1gXy2TnsGQnYYrm4mwEdeL06YaMhgVd9mgHYm5E6RzbYy7Bstkp4V-SarlDaQw1qSLN5hNsUep-q6AtGcTiapfhQhtFHnUZ73najuvOu4oxZoLvsb2lB8oMDQLwruCATmXlCh-_5TnXcvzr_Ss1sJTiI8E';
 
     try {
         const response = await fetch('https://accounts.spotify.com/api/token', {
